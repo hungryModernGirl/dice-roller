919baa753667c4646ffaf2aa2fa34b9fe39b5a60
kwilson <kwilson@optaros.com>
2015-02-05 14:12:41 -0600

    socket.io working and results are unique

--- app.js
+++ app.js
@@ -1,79 +1,64 @@
 /*
  * Module dependencies
  */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
-  , exphbs = require('express-handlebars')
-  , bodyparser = require('body-parser')
-  , http = require('http').Server(app)
-  , io = require('socket.io')(http)
+var express = require('express');
+var app = express();
+var stylus = require('stylus');
+var nib = require('nib');
+var exphbs = require('express-handlebars');
+var bodyparser = require('body-parser');
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
 
 
- var app = express();
+http.listen(3000);
+console.log('listening on 3000');
 
-function compile(str, path) {
+/* 
+ * Stylus styling setup
+ *
+ */
+ function compile(str, path) {
   return stylus(str)
     .set('filename', path)
-    .use(nib())
+    .use(nib());
 }
-app.set('views', __dirname + '/views')
-// app.set('view engine', 'jade')
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(express.logger('dev'))
 app.use(stylus.middleware(
   { src: __dirname + '/public'
   , compile: compile
   }
 ))
-app.use(express.static(__dirname + '/public'))
+
+/* 
+ * Handlebars templating setup
+ *
+ */
+app.set('views', __dirname + '/views');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.logger('dev'));
+
+app.use(express.static(__dirname + '/public'));
 app.use(bodyparser.urlencoded({extended: false}));
 
-// app.get('/', function (req, res) {
-//   res.render('dice',
-//   { title : 'dice!',
-//    post: req.body.str}
-//   )
-// })
+
+
 app.get('/', function (req, res) {
   res.render('dice.handlebars');
 })
-
-// app.post('/', function(req, res) {
-//   var post_array = req.body
-//   res.render('dice',
-//     { title: 'nice rollin',
-//     post: calculateRoll(post_array.sides, post_array.how_many)})
-// })
 
 app.post('/', function(req, res) {
   res.render('dice.handlebars', {result: calculateRoll(req.body.sides, req.body.how_many)});
 })
 
 io.on('connection', function(socket){
+  console.log('made connection');
   socket.on('dice roll', function(msg){
-    io.emit('dice roll', msg);
+    socket.emit('dice roll', msg);
   });
 });
 
-function calculateRoll(sides, how_many){
-  var total = 0;
-  for (i = 0; i < how_many; i++){
-    var rand = Math.floor((Math.random() * sides) + 1);
-    total += rand;
-    console.log(rand);
-  }
-  return total;
-}
 
-// exphbs.registerHelper("post", function(fn, req){
-//   var post_array = req.body; 
-//   $('div').html(fn(this))
 
-//   return calculateRoll(post_array.sides, post_array.how_many);
-// })
-
-app.listen(3000)
-
-console.log('listening on 3000')
