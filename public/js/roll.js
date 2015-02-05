0bb59b192b27e5b553f6f760bbb4d09daf4e65f2
kwilson <kwilson@optaros.com>
2015-02-05 14:30:08 -0600

    added more styling; additional js file; etc

---
+++ public/js/roll.js
@@ -0,0 +1,10 @@
+function calculateRoll(val_array){
+    var sides = val_array[0];
+    var how_many = val_array[1];
+    var total = 0;
+    for (i = 0; i < how_many; i++){
+      var rand = Math.floor((Math.random() * sides) + 1);
+      total += rand;
+    }
+    return total;
+}
